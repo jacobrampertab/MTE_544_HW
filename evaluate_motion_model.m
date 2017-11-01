3c601e28e3195d1c1cd2fd483113e6547bc878c7
jclundy <joseph.lundy456@gmail.com>
2017-11-01 18:10:06 -0400

    modifies motion model code to use correct math; first determine robot's relative velocity; then determine global velocity

--- evaluate_motion_model.m
+++ evaluate_motion_model.m
@@ -19,12 +19,12 @@ theta_new = theta + thetadot * T + angular_disturbance;
 %% Update x-y velocity of Robot
 % velocities at 3 wheel hubs
 v1 = r * w1; v2 = r * w2; v3 = r * w3;
-% absolute angular position of each wheel
-offsetAngle = 2*pi/3;
-a1 = theta; a2 = a1 + offsetAngle; a3 = a2 + offsetAngle;
-% summing x and y velocity components, with gaussian noise
-xdot = v1*cos(a1) + v2*cos(a2) + v3*cos(a3);
-ydot = v1*sin(a1) + v2*sin(a2) + v3*sin(a3);
+% calculate robot's velocity in its relative frame
+xdot_local = (v3-v2)*0.866; %cos(30) ~= 0.866
+ydot_local = v1 - (v3+v2)*0.5; %cos(60) = 0.5
+% calculate robot's velocity in its global frame
+xdot_global = cos(theta) * xdot_local - sin(theta)*ydot_local;
+ydot_global = sin(theta) * xdot_local + cos(theta)*xdot_local;
 
 %% Update x-y position of Robot
 % Gaussian Noise for positional disturbance
@@ -32,7 +32,7 @@ x_disturbance = normrnd(0,sigma_xy);
 y_disturbance = normrnd(0,sigma_xy);
 % updating x-y with gaussian noise
 x = Prev_state(1); y = Prev_state(2);
-x_new = x + xdot * T + x_disturbance;
-y_new = y + ydot * T + y_disturbance;
+x_new = x + xdot_global * T + x_disturbance;
+y_new = y + ydot_global * T + y_disturbance;
 %% Return Updated State Vector
-current_state = [x_new,y_new,xdot,ydot, theta_new, thetadot];
+current_state = [x_new,y_new,xdot_global,ydot_global, theta_new, thetadot];
