50dd8e154be2f752813296a15a6694d3b71677f8
jclundy <joseph.lundy456@gmail.com>
2017-11-01 17:45:14 -0400

    adds additional comments for next steps in EKF algorithm

--- MTE544_A1.m
+++ MTE544_A1.m
@@ -31,7 +31,21 @@ Y_graph = zeros(1,100);
 %% Outputting Motion
 for step=1:N
     time = step*t;
+    %1. evaluate true state
     State = evaluate_motion_model(State,Omega, r, l, time, sigma_theta, sigma_xy);
+    %2. evaluate measurement model based on state
+    % evaluate_sensor_model(...)
+    %% prediction update
+    % Evaluate G_matrix
+    % G_pred = evaluate_G(...) - note G is partial derivative wrt x of motion model g(x,u)
+    % Evaluate miu (predicted state based on previous predicted state and
+    % current input; based on motion model g(x,u))
+    % Evalute Sigma predicted G*Sigma_prev*transpose(G) + R ; R is
+    % convariance of motion model
+    %% Measurement update
+    %Evaluate H_matrix
+    % H_pre
+    %graphing
     x = State(1); y = State(2); theta = State(5);
     draw_three_wheel_bot(x,y,theta,l,1);
     X_graph(step) = x; Y_graph(step) = y;
