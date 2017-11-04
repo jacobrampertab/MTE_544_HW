6a96e995b7f629d849a6aeb6012b3f9f8723059e
jacobrampertab <jacobrampertab@gmail.com>
2017-11-03 21:16:04 -0400

    Reduce Motion Matrix
    Matricies for motion model reduced to 3x3 for reduced states

--- Jake_revision/evaluate_H.m
+++ Jake_revision/evaluate_H.m
@@ -4,11 +4,7 @@ function H= evaluate_H(X, u, T, r)
 % r - wheel radius
 % l - robot body radius
 % u - current input (w1, w2, w3)
-H = [
-    1 0 0 0 0 0;
-    0 0 0 0 0 0;
-    0 0 1 0 0 0;
-    0 0 0 0 0 0;
-    0 0 0 0 1 0;
-    0 0 0 0 0 0;
-    ]
+H = [1 0 0;
+     0 1 0;
+     0 0 1;
+    ];
