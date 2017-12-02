b5894f7b56b5502a5a903e7fc72ed950ebda42c5
Joseph Lundy <joseph.lundy456@gmail.com>
2017-12-02 18:02:35 -0500

    plots square trajectory

--- A2/question1.m
+++ A2/question1.m
@@ -24,69 +24,45 @@ tvec = 0:dt:T; % Time vector
 n = length(tvec); % Number of timesteps
 
 % Vehicle parameters
+r = 1; % Wheel radius
 l = 0.3; % Distance from wheel to center
 
-% Controller Parameters
-k = 1; % steering correction gain
-
-% carrot controller
-r = 1; % track one meter ahead
-
 %% Bicycle Model Commands
+% TODO 
+% [x] length: 30 cm
+% [x] gaussian disturbance on x, y position : 0.02 m std dev
+% [x] gaussian disturbance on angle theta:  1 degree std dev (0.01744)
+% [x] update interval : 0.1 s
+% [x] speed: 3 m/s
+% [x] steering angle : delta = 10 - t degrees
+% [x] steering angle limits : +/- 30 degrees (+/-0.523 rads)
+% [x] 20 seconds
 
 % Speed command
 v = 3*ones(1,n); % Constant throughout
 
 % Steering Angle
-delta = zeros(1,n); %in rad
-
-% Heading errors
-dHeadErr = 0;
-curHeadErr = 0;
-prevHeadErr = 0;
-
-%% Waypoints
-waypoints = [20 0; 20 5; 0 5; 0 0];
-numWayPoints = length(waypoints);
-prevWayPoint = 4;
-curWayPoint = 1;
-
-%% Body motion integration
-x = zeros(3,n);
-disp('Bicycle Car Simulation')
-for t=1:n-1
-    % TODO use r here
-    [crosstrack_error outside] = distanceToLineSegment(waypoints(prevWayPoint,:), waypoints(curWayPoint,:), x(1:2,t)');
-    if(outside)
-        prevWayPoint = curWayPoint;
-        curWayPoint = curWayPoint + 1;
-        if(curWayPoint > numWayPoints)
-            curWayPoint = 1;
-        end
-        dHeadErr(t+1) = 0;
-        headingErr(t+1) = pi()/2;
-        
-    else 
-        dHeadErr = -v(t+1)*sin(delta(t))/l;
-        headingErr(t+1) = headingErr(t) + dHeadErr*dt;
-        delta(t+1) = headingErr(t+1) + atan(k*crosstrack_error/v(t+1));
-
-        if(delta(t+1) > 0.523)
-            delta(t+1) = 0.523;
-        end
-        if(delta(t+1) < -0.523)
-            delta(t+1) = -0.523;
-        end 
-
-        % Update next state
-        x(:,t+1) = bicycle(x(:,t),v(t),delta(t+1),l,dt);
-        %noise = [normrnd(0, 0.02) ; normrnd(0, 0.02) ; normrnd(0,0.01744)];
-        %x(:,t+1) =  x(:,t+1)+ noise;
+delta = 10*ones(1,n); %in degrees
+delta = delta - tvec; % apply steering angle model delta = 10 - t
+delta = delta * pi()/180; % convert to radians
+for t=1:n
+    if(delta(n) > 0.523)
+        delta(n) = 0.523;
     end
+    if(delta(n) < -0.523)
+        delta(n) = -0.523;
+    end 
 end
 
-
-%% Create figure of motion
+%% Body motion integration
+x = [0; 0; 0];
+disp('Bicycle Car Simulation')
+for t=1:n
+    x(:,t+1) = bicycle(x(:,t),v(t),delta(t),l,dt);
+    noise = [normrnd(0, 0.02) ; normrnd(0, 0.02) ; normrnd(0,0.01744)];
+    x(:, t+1) =  x(:, t+1)+ noise;
+end
+% Create figure of motion
 figure(1);
 for t=1:2:n % For every second position of the robot
     clf;hold on; % clear the current figure
@@ -95,7 +71,7 @@ for t=1:2:n % For every second position of the robot
     
     plot(x(1,1:t), x(2,1:t), 'bx'); % Draw the path of the vehicle
     axis equal; % Use same scale on both axes
-    %axis([-15 10 -25 5]); % Set the dimensions of the plot
+    axis([-15 10 -25 5]); % Set the dimensions of the plot
     drawnow; % Draw the plot now instead of when Matlab feels like it
     if video 
         writeVideo(vidObj, getframe(gcf)); % Record the plot as a frame in the movie
